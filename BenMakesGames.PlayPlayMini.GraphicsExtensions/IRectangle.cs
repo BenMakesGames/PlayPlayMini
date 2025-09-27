7ac4f535035e9c9623a701e146739f639b610d61
Ben <ben@benmakesgames.com>
2025-09-27 13:56:05 -0700

    more rectangle stuff!

--- BenMakesGames.PlayPlayMini.GraphicsExtensions/IRectangle.cs
+++ BenMakesGames.PlayPlayMini.GraphicsExtensions/IRectangle.cs
@@ -1,5 +1,6 @@
 ﻿using System.Numerics;
 using BenMakesGames.PlayPlayMini.Services;
+using Microsoft.Xna.Framework;
 
 namespace BenMakesGames.PlayPlayMini.GraphicsExtensions;
 
@@ -24,7 +25,7 @@ public static class RectangleExtensions
     /// Checks if a rectangle contains the current mouse position.
     /// </summary>
     /// <param name="rectangle">The rectangle to check.</param>
-    /// <param name="mouse">The mouse manager instance.</param>
+    /// <param name="mouse">The <see cref="MouseManager">MouseManager</see> instance.</param>
     /// <returns>True if the rectangle contains the mouse position, false otherwise.</returns>
     public static bool Contains(this IRectangle<int> rectangle, MouseManager mouse) =>
         rectangle.Contains(mouse.X, mouse.Y);
@@ -33,7 +34,7 @@ public static class RectangleExtensions
     /// Checks if a rectangle contains the current mouse position.
     /// </summary>
     /// <param name="rectangle">The rectangle to check.</param>
-    /// <param name="mouse">The mouse manager instance.</param>
+    /// <param name="mouse">The <see cref="MouseManager">MouseManager</see> instance.</param>
     /// <returns>True if the rectangle contains the mouse position, false otherwise.</returns>
     public static bool Contains(this IRectangle<float> rectangle, MouseManager mouse) =>
         rectangle.Contains(mouse.X, mouse.Y);
@@ -42,7 +43,7 @@ public static class RectangleExtensions
     /// Checks if a rectangle contains the current mouse position.
     /// </summary>
     /// <param name="rectangle">The rectangle to check.</param>
-    /// <param name="mouse">The mouse manager instance.</param>
+    /// <param name="mouse">The <see cref="MouseManager">MouseManager</see> instance.</param>
     /// <returns>True if the rectangle contains the mouse position, false otherwise.</returns>
     public static bool Contains(this IRectangle<double> rectangle, MouseManager mouse) =>
         rectangle.Contains(mouse.X, mouse.Y);
@@ -51,7 +52,7 @@ public static class RectangleExtensions
     /// Checks if the mouse is within the graphics viewport.
     /// </summary>
     /// <param name="graphics">The graphics manager instance.</param>
-    /// <param name="mouse">The mouse manager instance.</param>
+    /// <param name="mouse">The <see cref="MouseManager">MouseManager</see> instance.</param>
     /// <returns>True if the mouse is within the viewport, false otherwise.</returns>
     public static bool Contains(this GraphicsManager graphics, MouseManager mouse) =>
         mouse.X >= 0 && mouse.X < graphics.Width &&
@@ -68,4 +69,53 @@ public static class RectangleExtensions
     public static bool Contains<T>(this IRectangle<T> rectangle, T x, T y) where T: INumber<T> =>
         x >= rectangle.X && x < rectangle.X + rectangle.Width &&
         y >= rectangle.Y && y < rectangle.Y + rectangle.Height;
+
+    /// <summary>
+    /// Checks if a rectangle contains a point specified by coordinates.
+    /// </summary>
+    /// <param name="rectangle">The rectangle to check.</param>
+    /// <param name="x">X coordinate of the point.</param>
+    /// <param name="y">Y coordinate of the point.</param>
+    /// <returns>True if the rectangle contains the point, false otherwise.</returns>
+    public static bool Contains(this Rectangle rectangle, int x, int y) =>
+        x >= rectangle.X && x < rectangle.X + rectangle.Width &&
+        y >= rectangle.Y && y < rectangle.Y + rectangle.Height;
+
+    /// <summary>
+    /// Checks if the mouse is within the graphics viewport.
+    /// </summary>
+    /// <param name="rectangle">The rectangle to check.</param>
+    /// <param name="mouse">The <see cref="MouseManager">MouseManager</see> instance.</param>
+    /// <returns>True if the rectangle contains the point, false otherwise.</returns>
+    public static bool Contains(this Rectangle rectangle, MouseManager mouse) =>
+        mouse.X >= rectangle.X && mouse.X < rectangle.X + rectangle.Width &&
+        mouse.Y >= rectangle.Y && mouse.Y < rectangle.Y + rectangle.Height;
+
+    /// <summary>
+    /// Draws the outline of a rectangle.
+    /// </summary>
+    /// <param name="graphics"></param>
+    /// <param name="rectangle"></param>
+    /// <param name="outlineColor"></param>
+    public static void DrawRectangle(this GraphicsManager graphics, IRectangle<int> rectangle, Color outlineColor)
+        => graphics.DrawRectangle(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height, outlineColor);
+
+    /// <summary>
+    /// Draws a filled rectangle.
+    /// </summary>
+    /// <param name="graphics"></param>
+    /// <param name="rectangle"></param>
+    /// <param name="fillColor"></param>
+    public static void DrawFilledRectangle(this GraphicsManager graphics, IRectangle<int> rectangle, Color fillColor)
+        => graphics.DrawFilledRectangle(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height, fillColor);
+
+    /// <summary>
+    /// Draws a filled rectangle.
+    /// </summary>
+    /// <param name="graphics"></param>
+    /// <param name="rectangle"></param>
+    /// <param name="fillColor"></param>
+    /// <param name="outlineColor"></param>
+    public static void DrawFilledRectangle(this GraphicsManager graphics, IRectangle<int> rectangle, Color fillColor, Color outlineColor)
+        => graphics.DrawFilledRectangle(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height, fillColor, outlineColor);
 }
--- BenMakesGames.PlayPlayMini/Services/GraphicsManager.Rectangles.cs
+++ BenMakesGames.PlayPlayMini/Services/GraphicsManager.Rectangles.cs
@@ -1,43 +1,98 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using System.Runtime.CompilerServices;
 
 namespace BenMakesGames.PlayPlayMini.Services;
 
 public sealed partial class GraphicsManager
 {
+    /// <summary>
+    /// Draws a filled rectangle.
+    /// </summary>
+    /// <param name="upperLeft"></param>
+    /// <param name="bottomRight"></param>
+    /// <param name="fillColor"></param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void DrawFilledRectangle((int x, int y) upperLeft, (int x, int y) bottomRight, Color c)
+    public void DrawFilledRectangle((int x, int y) upperLeft, (int x, int y) bottomRight, Color fillColor)
     {
-        SpriteBatch.Draw(WhitePixel, new Rectangle(upperLeft.x, upperLeft.y, bottomRight.x - upperLeft.x + 1, bottomRight.y - upperLeft.y + 1), null, c);
+        SpriteBatch.Draw(WhitePixel, new Rectangle(upperLeft.x, upperLeft.y, bottomRight.x - upperLeft.x + 1, bottomRight.y - upperLeft.y + 1), null, fillColor);
         DrawCalls++;
     }
 
+    /// <summary>
+    /// Draws a filled rectangle.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="w"></param>
+    /// <param name="h"></param>
+    /// <param name="fillColor"></param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void DrawFilledRectangle(int x, int y, int w, int h, Color c)
+    public void DrawFilledRectangle(int x, int y, int w, int h, Color fillColor)
     {
-        SpriteBatch.Draw(WhitePixel, new Rectangle(x, y, w, h), null, c);
+        SpriteBatch.Draw(WhitePixel, new Rectangle(x, y, w, h), null, fillColor);
         DrawCalls++;
     }
 
+    /// <summary>
+    /// Draws a filled rectangle.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="w"></param>
+    /// <param name="h"></param>
+    /// <param name="fillColor"></param>
+    /// <param name="outlineColor"></param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void DrawFilledRectangle(int x, int y, int w, int h, Color fill, Color outline)
+    public void DrawFilledRectangle(int x, int y, int w, int h, Color fillColor, Color outlineColor)
     {
-        DrawFilledRectangle(x + 1, y + 1, w - 2, h - 2, fill);
-        DrawRectangle(x, y, w, h, outline);
+        DrawFilledRectangle(x + 1, y + 1, w - 2, h - 2, fillColor);
+        DrawRectangle(x, y, w, h, outlineColor);
+    }
+
+    /// <summary>
+    /// Draws the outline of a rectangle.
+    /// </summary>
+    /// <param name="rectangle"></param>
+    /// <param name="outlineColor"></param>
+    public void DrawRectangle(Rectangle rectangle, Color outlineColor)
+        => DrawRectangle(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height, outlineColor);
+
+    /// <summary>
+    /// Draws a filled rectangle.
+    /// </summary>
+    /// <param name="rectangle"></param>
+    /// <param name="fillColor"></param>
+    public void DrawFilledRectangle(Rectangle rectangle, Color fillColor)
+    {
+        SpriteBatch.Draw(WhitePixel, rectangle, null, fillColor);
+        DrawCalls++;
+    }
+
+    /// <summary>
+    /// Draws a filled rectangle.
+    /// </summary>
+    /// <param name="rectangle"></param>
+    /// <param name="fillColor"></param>
+    /// <param name="outlineColor"></param>
+    public void DrawFilledRectangle(Rectangle rectangle, Color fillColor, Color outlineColor)
+    {
+        DrawFilledRectangle(rectangle, fillColor);
+        DrawRectangle(rectangle, outlineColor);
     }
 
     /// <summary>
     /// Draw a series of points in the given color.
     /// </summary>
     /// <param name="points"></param>
-    /// <param name="c"></param>
+    /// <param name="color"></param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void DrawPoints(IEnumerable<(int x, int y)> points, Color c)
+    [Obsolete("This method will be moved to the PlayPlayMini.GraphicsExtensions package.")]
+    public void DrawPoints(IEnumerable<(int x, int y)> points, Color color)
     {
         foreach (var p in points)
-            DrawPoint(p.x, p.y, c);
+            DrawPoint(p.x, p.y, color);
     }
 
     /// <summary>
@@ -47,24 +102,30 @@ public sealed partial class GraphicsManager
     /// <param name="y"></param>
     /// <param name="w"></param>
     /// <param name="h"></param>
-    /// <param name="c"></param>
-    public void DrawRectangle(int x, int y, int w, int h, Color c)
+    /// <param name="outlineColor"></param>
+    public void DrawRectangle(int x, int y, int w, int h, Color outlineColor)
     {
         // top & bottom line
-        SpriteBatch.Draw(WhitePixel, new Rectangle(x, y, w, 1), null, c);
-        SpriteBatch.Draw(WhitePixel, new Rectangle(x, y + h - 1, w, 1), null, c);
+        SpriteBatch.Draw(WhitePixel, new Rectangle(x, y, w, 1), null, outlineColor);
+        SpriteBatch.Draw(WhitePixel, new Rectangle(x, y + h - 1, w, 1), null, outlineColor);
 
         // left & right line
-        SpriteBatch.Draw(WhitePixel, new Rectangle(x, y + 1, 1, h - 2), null, c);
-        SpriteBatch.Draw(WhitePixel, new Rectangle(x + w - 1, y + 1, 1, h - 2), null, c);
+        SpriteBatch.Draw(WhitePixel, new Rectangle(x, y + 1, 1, h - 2), null, outlineColor);
+        SpriteBatch.Draw(WhitePixel, new Rectangle(x + w - 1, y + 1, 1, h - 2), null, outlineColor);
 
         DrawCalls += 4;
     }
 
+    /// <summary>
+    /// Draws a single point.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="color"></param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void DrawPoint(int x, int y, Color c)
+    public void DrawPoint(int x, int y, Color color)
     {
-        SpriteBatch.Draw(WhitePixel, new Rectangle(x, y, 1, 1), c);
+        SpriteBatch.Draw(WhitePixel, new Rectangle(x, y, 1, 1), color);
         DrawCalls++;
     }
 }
