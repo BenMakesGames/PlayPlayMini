63526afbce8267665cd44cdb2d222951de4dde39
Ben H <ben@benmakesgames.com>
2026-01-31 14:47:12 -0800

    giving IAsset a public Key property

--- BenMakesGames.PlayPlayMini/Model/IAsset.cs
+++ BenMakesGames.PlayPlayMini/Model/IAsset.cs
@@ -1,3 +1,10 @@
 ﻿namespace BenMakesGames.PlayPlayMini.Model;
 
-public interface IAsset;
+public interface IAsset
+{
+    /// <summary>
+    /// An identifier for this asset. Must be unique within its type. (For example, a Picture and a
+    /// SpriteSheet may have the same Key, but two Pictures may not have the same Key.)
+    /// </summary>
+    string Key { get; }
+}
